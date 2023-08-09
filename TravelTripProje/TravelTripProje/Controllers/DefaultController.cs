9b7c887c97371ebb2f2a3d5d49779a820654e096
Yavuz Selim Sarı <yavuzselimsariii@gmail.com>
2023-08-10 01:57:45 +0300

    TravelTripWeb

---
+++ TravelTripProje/TravelTripProje/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+namespace TravelTripProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }   
+        public PartialViewResult Partial1()
+        {
+            var deger=c.Blogs.Take(5).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x=>x.ID).ToList();
+            return PartialView(deger);
+        }
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Models/Siniflar/AnaSayfa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class AnaSayfa
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
